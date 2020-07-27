ebabeb9ddce35fa3dca2201973d0460711c2f2a2
TikumpornTuksanant <56583565+TikumpornTuksanant@users.noreply.github.com>
2020-07-27 18:30:33 +0700

    Step 2: Build and call an implicit intent

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,11 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.Settings.Global.getString
+import android.view.*
+import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -40,4 +41,33 @@ class GameWonFragment : Fragment() {
         setHasOptionsMenu(true)
         return binding.root
     }
+}
+
+private fun getShareIntent() : Intent {
+    val args = GameWonFragmentArgs.fromBundle(arguments!!)
+    val shareIntent = Intent(Intent.ACTION_SEND)
+    shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+    return shareIntent
+}
+private fun shareSuccess() {
+    startActivity(getShareIntent())
+}
+
+override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+    super.onCreateOptionsMenu(menu, inflater)
+    inflater?.inflate(R.menu.winner_menu, menu)
+    // check if the activity resolves
+    if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+        // hide the menu item if it doesn't resolve
+        menu?.findItem(R.id.share)?.setVisible(false)
+    }
+}
+
+// Sharing from the Menu
+override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item!!.itemId) {
+        R.id.share -> shareSuccess()
+    }
+    return super.onOptionsItemSelected(item)
 }
