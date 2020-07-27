7f4bed6ddb54973fbc5bd8028482cd09e62fb4d1
TikumpornTuksanant <56583565+TikumpornTuksanant@users.noreply.github.com>
2020-07-27 16:11:53 +0700

    Step 1: Add the Material library to your project

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
-        
+
     }
 
 }
